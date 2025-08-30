3692d0d7db812a603f1133982e17cac08c4cefce
= <=>
2025-08-30 16:28:21 +0800

    v250830

---
+++ Untitled1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+	cout<<"   *   "<<endl;
+	cout<<"  ***  "<<endl;
+	cout<<" ***** "<<endl;
+	cout<<"*******"<<endl;
+	cout<<135/5;
+	cout<<136%5;
+	cout<<135*5;
+	cout<<135+5;
+	cout<<135-5;
+	cout<<125/8;
+	int a;
+	cin>>a; //put255
+	cout<<a%10 + a/100 + a/10%10;
+	return 520;
+}
